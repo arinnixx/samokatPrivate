4e17bdce7fd5796da665104464ad458e64f039e3
a.kaneva <a.kaneva@iac.spb.ru>
2025-10-28 13:06:11 +0300

    правки

--- src/delivery-status/delivery-status.module.ts
+++ src/delivery-status/delivery-status.module.ts
@@ -3,13 +3,16 @@ import {DeliveryStatusService} from './delivery-status.service';
 import {DeliveryStatusController} from './delivery-status.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {DeliveryStatus} from "../entities/DeliveryStatus";
+import { RabbitmqModule } from '../rabbitmq/rabbitmq.module';
+import { RabbitmqService } from '../rabbitmq/rabbitmq.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([DeliveryStatus]),
+        RabbitmqModule
     ],
     controllers: [DeliveryStatusController],
-    providers: [DeliveryStatusService],
+    providers: [DeliveryStatusService,RabbitmqService],
     exports: [DeliveryStatusService],
 })
 export class DeliveryStatusModule {
--- src/delivery-status/delivery-status.service.ts
+++ src/delivery-status/delivery-status.service.ts
@@ -1,16 +1,19 @@
 import {Injectable} from '@nestjs/common';
-import {BaseService} from "../base/base.service";
 import {DeliveryStatus} from "../entities/DeliveryStatus";
 import {InjectRepository} from "@nestjs/typeorm";
 import {DataSource, Repository} from "typeorm";
+import { RabbitmqService } from '../rabbitmq/rabbitmq.service';
+import { BasePrivateService } from '../base/base-private.service';
 
 @Injectable()
-export class DeliveryStatusService extends BaseService<DeliveryStatus> {
+export class DeliveryStatusService extends BasePrivateService<DeliveryStatus> {
+    name = 'delivery-status';
     constructor(
         @InjectRepository(DeliveryStatus) repo: Repository<DeliveryStatus>,
         dataSource: DataSource,
+        rmqService: RabbitmqService,
     ) {
-        super(repo, dataSource);
+        super(repo, dataSource, rmqService);
     }
 
 }
